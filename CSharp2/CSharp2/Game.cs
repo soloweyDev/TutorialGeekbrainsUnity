51f0d3d084b05bcdbe955f55fb409e256b4c398f
soloweyDev <solowey@narod.ru>
2019-08-09 11:47:21 +0300

    дз2ч2

--- CSharp2/CSharp2/Game.cs
+++ CSharp2/CSharp2/Game.cs
@@ -1,18 +1,21 @@
 ﻿using System;
-using System.Drawing;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace CSharp2
 {
     static class Game
     {
+        static BaseObject[] objs;
+        static Bullet bullet;
+        static Asteroid[] asteroids;
+
         static BufferedGraphicsContext context;
         static public BufferedGraphics buffer;
+        static public Random rnd = new Random();
 
-        public static int Width { get; set; }
-        public static int Height { get; set; }
-
-        static BaseObject[] objs;
+        static public int Width { get; set; }
+        static public int Height { get; set; }
 
         static Game()
         {
@@ -20,49 +23,55 @@ namespace CSharp2
 
         static public void Init(Form form)
         {
-            Width = form.Width - 10 - 10;
-            Height = form.Height - 32 - 10;
-
             Graphics g;
             context = BufferedGraphicsManager.Current;
             g = form.CreateGraphics();
+            Width = form.Width - 10 - 10;
+            Height = form.Height - 32 - 10;
             buffer = context.Allocate(g, new Rectangle(0, 0, Width, Height));
-
             Load();
-
             Timer timer = new Timer();
             timer.Interval = 100;
-            timer.Start();
             timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
         }
 
         static public void Load()
         {
-            objs = new BaseObject[44];
-            for (int i = 0; i < 8; i++)
-                objs[i] = new BaseObject(new Point(400, i * 20 + 20), new Point(15-i, 15-i), new Size(20, 20));
-
-            for (int i = 8; i < 16; i++)
-                objs[i] = new RectangleO(new Point(400, i * 20 + 20), new Point(15 - i, 15 - i), new Size(20, 20));
-
-            for (int i = 16; i < 24; i++)
-                objs[i] = new PolygonO(new Point(400, i * 20 + 20), new Point(15 - i, 15 - i), new Size(20, 20));
-
-            for (int i = 24; i < 44; ++i)
+            objs = new BaseObject[30];
+            asteroids = new Asteroid[3];
+            for (int i = 0; i < objs.Length; i++)
             {
-                int x = StarSpace.Rnd.Next(20, Width + 50);
-                int y = StarSpace.Rnd.Next(20, Height - 20);
-                objs[i] = new StarSpace(new Point(x, y), new Point(-3, 0), new Size(4, 4));
+                int r = rnd.Next(5, 50);
+                objs[i] = new Star(new Point(500, Game.rnd.Next(0, Game.Height)), new Point(-r, 0), new Size(3, 3));
             }
+            int h = 0;
+            for (int i = 0; i < asteroids.Length; i++)
+            {
+                int r = rnd.Next(5, 50);
+                h = Game.rnd.Next(0, Game.Height);
+                asteroids[i] = new Asteroid(new Point(800, h), new Point(-5, 0), new Size(r, r));
+            }
+            bullet = new Bullet(new Point(0, h), new Point(5, 0), new Size(4, 1));
         }
-
         static public void Draw()
         {
             buffer.Graphics.Clear(Color.Black);
-
-            foreach (BaseObject obj in objs)
+            //buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+            foreach(BaseObject obj in objs)
+            {
                 obj.Draw();
-
+            }
+            foreach (Asteroid obj in asteroids)
+                obj.Draw();
+            bullet.Draw();
             buffer.Render();
         }
 
@@ -70,12 +79,20 @@ namespace CSharp2
         {
             foreach (BaseObject obj in objs)
                 obj.Update();
-        }
+            for (int i = 0; i < asteroids.Length; ++i)
+            {
+                asteroids[i].Update();
+                if (asteroids[i].Collision(bullet))
+                {
+                    System.Media.SystemSounds.Hand.Play();
+                    bullet = new Bullet(new Point(0, 200), new Point(5, 0), new Size(4, 1));
+                    int r = rnd.Next(5, 50);
+                    asteroids[i] = new Asteroid(new Point(800, 200), new Point(-5, 0), new Size(r, r));
+                }
+            }
+            bullet.Update();
 
-        private static void Timer_Tick(object sender, EventArgs e)
-        {
-            Draw();
-            Update();
+
         }
     }
 }
--- CSharp2/CSharp2/Objects.cs
+++ CSharp2/CSharp2/Objects.cs
@@ -3,7 +3,13 @@ using System.Drawing;
 
 namespace CSharp2
 {
-    class BaseObject
+    interface ICollision
+    {
+        
+        bool Collision(ICollision obj);
+        Rectangle Rect { get; }
+    }
+    abstract class BaseObject:ICollision
     {
         protected Point pos;
         protected Point dir;
@@ -16,62 +22,113 @@ namespace CSharp2
             this.size = size;
         }
 
-        public virtual void Draw()
+        public abstract void Draw();
+
+        public abstract void Update();
+        //{
+        //    pos.X = pos.X + dir.X;
+        //    pos.Y = pos.Y + dir.Y;
+        //    if (pos.X < 5 || pos.X > Game.Width - 20) dir.X = -dir.X;
+        //    if (pos.Y < 5 || pos.Y > Game.Height - 20) dir.Y = -dir.Y;
+        //}
+
+        public override string ToString()
         {
-            Game.buffer.Graphics.DrawEllipse(Pens.White, pos.X, pos.Y, size.Width, size.Height);
+            return "Pos:" + pos.ToString() + " Dir" + dir.ToString() + " Size:" + size.ToString();
         }
 
-        public virtual void Update()
+
+        public bool Collision(ICollision o)
         {
-            pos.X = pos.X + dir.X;
-            pos.Y = pos.Y + dir.Y;
-            if (pos.X < 5 || pos.X > Game.Width - 20) dir.X = -dir.X;
-            if (pos.Y < 5 || pos.Y > Game.Height - 20) dir.Y = -dir.Y;
+            return o.Rect.IntersectsWith(Rect);
+        }
+
+        public Rectangle Rect
+        {
+            get { return new Rectangle(pos, size); }
         }
     }
 
-    class RectangleO : BaseObject
+    class Star: BaseObject
     {
-        public RectangleO(Point pos, Point dir, Size size) : base(pos, dir, size)
+        Image img = Image.FromFile("pictures\\star.png"); 
+
+        public Star(Point pos, Point dir, Size size):base(pos,dir,size)
         {
+
         }
 
         public override void Draw()
         {
-            Game.buffer.Graphics.DrawRectangle(Pens.White, pos.X, pos.Y, size.Width, size.Height);
-        }
-    }
-
-    class PolygonO : BaseObject
-    {
-        public PolygonO(Point pos, Point dir, Size size) : base(pos, dir, size)
-        {
-        }
-
-        public override void Draw()
-        {
-            Game.buffer.Graphics.DrawPolygon(Pens.White, new PointF[] { new PointF(pos.X, pos.Y), new PointF(size.Width, size.Height), new PointF(pos.X, size.Height) });
-        }
-    }
-
-    class StarSpace : BaseObject
-    {
-        public static Random Rnd { get; set; } = new Random();
-
-        public StarSpace(Point pos, Point dir, Size size) : base(pos, dir, size)
-        {
-        }
-
-        public override void Draw()
-        {
-            Game.buffer.Graphics.FillEllipse(Brushes.White, new RectangleF(pos.X, pos.Y, size.Width, size.Height));
+            //Game.buffer.Graphics.DrawRectangle(Pens.White, pos.X, pos.Y, size.Width, size.Height);
+            Game.buffer.Graphics.DrawImage(img, pos);
         }
 
         public override void Update()
         {
+            //base.Update();
             pos.X = pos.X + dir.X;
-            pos.Y = pos.Y + dir.Y;
-            if (pos.X < 0) pos.X = Game.Width;
+            //pos.Y = pos.Y + dir.Y;
+            if (pos.X < 0) //dir.X = -dir.X;
+            {
+                pos.X = 1000;
+                pos.Y = Game.rnd.Next(0, Game.Height);
+                dir.X = -Game.rnd.Next(1, 10);
+            }
+            //if (pos.X > Game.Width) dir.X = -dir.X;
+            //if (pos.Y < 0) dir.Y = -dir.Y;
+            //if (pos.Y > Game.Height) dir.Y = -dir.Y;
         }
     }
+
+    class Asteroid: BaseObject
+    {
+        int power;
+
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+
+        public Asteroid(Point pos, Point dir, Size size): base(pos,dir,size)
+        {
+            Power = 1;
+        }
+
+        public override void Draw()
+        {
+            Game.buffer.Graphics.FillEllipse(Brushes.Wheat, pos.X, pos.Y, size.Width, size.Height);
+        }
+        public override void Update()
+        {
+            pos.X = pos.X + dir.X;
+            if (pos.X < 0) //dir.X = -dir.X;
+            {
+                pos.X = 1000;
+                pos.Y = Game.rnd.Next(0, Game.Height);
+                dir.X = -Game.rnd.Next(1, 10);
+            }
+        }
+
+    }
+
+    class Bullet : BaseObject
+    {
+        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.buffer.Graphics.DrawRectangle(Pens.OrangeRed, pos.X, pos.Y, size.Width, size.Height);
+        }
+
+        public override void Update()
+        {
+            pos.X = pos.X + 3;
+        }
+    }
+
+
 }
