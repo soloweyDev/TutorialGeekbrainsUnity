e485b44441a00f06c98ead96cbd4923dda53a6bf
soloweyDev <solowey@narod.ru>
2019-08-26 15:42:18 +0300

    ЗД 4 п.3
    С клавиатуры вводится скобочное выражение. Требуется проверить правильность расстановки скобок.(Использовать обобщенную коллекцию Stack<char>)

---
+++ CSharp2/DZ2Ch4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DZ2Ch4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+
+        // С клавиатуры вводится скобочное выражение. Требуется проверить правильность расстановки скобок.(Использовать обобщенную коллекцию Stack<char>)
+        static void Task1()
+        {
+            Console.WriteLine("Введите скобочное выражение");
+            string input = Console.ReadLine();
+            Stack<char> parentheses = new Stack<char>();
+            bool result = true;
+
+            foreach (char ch in input)
+            {
+                if (ch == '(')
+                {
+                    parentheses.Push(ch);
+                }
+                else if (ch == ')')
+                {
+                    if (parentheses.Count == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                    parentheses.Pop();
+                }
+            }
+
+            if (!result || parentheses.Count != 0) Console.WriteLine("Число скобок не совпадает");
+            else Console.WriteLine("Число скобок совпадает");
+        }
+    }
+}
