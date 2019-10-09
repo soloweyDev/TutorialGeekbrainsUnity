90aed80442e8ca13b8b85d8aad6556a47125bb25
soloweyDev <solowey@narod.ru>
2019-10-09 17:08:24 +0300

    Урок 8

---
+++ CSharp2/DZ2Ch8_Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DZ2Ch8_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.StoclService service = new localhost.StoclService();
+            bool res;
+            double price;
+            service.GetPrice("100", out price, out res);
+
+            Console.WriteLine(price);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp2/DZ2Ch8_Host/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace DZ2Ch8_Host
+{
+    [ServiceContract]
+    public interface ISockService
+    {
+        [OperationContract]
+        double GetPrice(string tiker);
+    }
+
+    public class StoclService : ISockService
+    {
+        public double GetPrice(string tiker)
+        {
+            return 94.85;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(StoclService), new Uri("http://localhost:8080/EssentialWCF"));
+            serviceHost.AddServiceEndpoint(typeof(ISockService), new BasicHttpBinding(), "");
+
+            ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+            behavior.HttpGetEnabled = true;
+            serviceHost.Description.Behaviors.Add(behavior);
+            serviceHost.AddServiceEndpoint(typeof(IMetadataExchange), MetadataExchangeBindings.CreateMexHttpBinding(), "mex");
+
+            serviceHost.Open();
+            Console.WriteLine("Для завершения нажмите <ENTER>.\n\n");
+            Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
